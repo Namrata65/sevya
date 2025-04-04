ede61c6c7fd56bf59ebd6af98ab7356013f1c670
Namrata65 <namrata.aeri@gmail.com>
2025-04-04 01:46:43 -0700

    landing page sevya

---
+++ soursce.js
@@ -0,0 +1,6 @@
+document.getElementById("menu-toggle").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("nav-menu").classList.toggle("active");
+});
+
+
